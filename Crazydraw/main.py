5802b584a2935af18ee209f4ee3db94590286436
Lorenzo Venerandi <68255980+Lore09@users.noreply.github.com>
2022-10-10 21:52:53 +0200

    Spline button

--- Crazydraw/main.py
+++ Crazydraw/main.py
@@ -13,8 +13,6 @@ window.setFixedSize(settings.get_paint_size_scaled()[0] + 500, 800)
 
 apply_stylesheet(app, theme='dark_red.xml')
 
-DrawSpline.plot_cubic_spline("saves/palle.csv")
-
 window.show()
 
 app.exec_()
